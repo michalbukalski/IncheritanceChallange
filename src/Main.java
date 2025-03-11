80cb10024057177a9a58a1be02be51d6585aafab
michalbukalski <michalbukalski@gmail.com>
2025-03-12 00:31:54 +0100

    another objects

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,18 @@ public class Main {
         System.out.println("Age = "+tim.getAge());
         System.out.println("Pay = "+tim.collectPay());
 
-        Employee joe = new Employee("Joe", "11/05/1990", "03/03/2021");
+        SalariedEmployee joe = new SalariedEmployee("Joe", "11/05/1990", "03/03/2021", 35000);
         System.out.println(joe);
-        System.out.println("Age = "+joe.getAge());
-        System.out.println("Pay = "+joe.collectPay());
+        System.out.println("Joe's Paycheck = $" +joe.collectPay());
+//        System.out.println("Age = "+joe.getAge());
+//        System.out.println("Pay = "+joe.collectPay());
+
+        joe.retire();
+        System.out.println("Joe's Pension check = $" + joe.collectPay());
+
+        HourlyEmployee mary = new HourlyEmployee("Mary", "05/05/1970", "03.01.2022", 15);
+        System.out.println(mary);
+        System.out.println("Mary's Paycheck = $" + mary.collectPay());
+        System.out.println("Mary's Holiday Pay = $" + mary.getDoublePay());
     }
 }
